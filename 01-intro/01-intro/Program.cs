92c0860cb7f4e0cad22665ef0df52c13ba9bc94b
gurban <98018715+gurbangurbanzade@users.noreply.github.com>
2023-08-26 14:05:10 +0400

    c# kodlari elave olundu

---
+++ 01-intro/01-intro/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("hello world");
+
+            //int num1 = 5;
+            //string soz1 = "baku";
+            //bool veziyyet = true;
+
+
+
+            //Console.WriteLine("num1-"+"       "+num1 + "soz1----"+soz1);
+            //string soz2 = Console.ReadLine();
+            //string soz3 = "123";
+            //int num2=Convert.ToInt32(soz2);
+            //// soz2= "123"
+            //Console.WriteLine("num2 tipi-" + num2.GetType());
+
+            //Console.WriteLine("num2-"+num2);
+
+
+            // operator + - * / % +=  && ||
+
+
+            //int num1 = 6;
+            //int num2 = 3;
+            //int result;
+            //bool res;
+            //result = num1+num2;
+            //result = num1 % 2;
+
+            //num1 += 3;
+
+
+
+            // num1= num1+3
+            //       true       
+            //res = num1 > num2 && num1 > 0;
+
+            //res = num1 > num2 || num1 < 0;
+
+            //bool res = 5!=6;
+
+
+            //Console.WriteLine("result-" + res);
+
+            //int num1 =5;
+            //int num2 = 5;
+
+            //if (num2>num1)
+            //{
+            //    Console.WriteLine("num2 num1-den boyukdur");
+            //}
+            //else
+            //if (num2 < num1)
+            //{
+            //    Console.WriteLine("num1 num2-den boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("num1 num2 beraberdir");
+            //}
+
+            //int reqem = 0;
+
+            //if (reqem>0)
+            //{
+            //    Console.WriteLine("musber");
+            //}
+            //else if(reqem<0) {
+
+
+            //    Console.WriteLine("menfi");
+            //} else {
+
+
+            //    Console.WriteLine("sifirdir");
+
+            //}
+            //Console.Write(soz1);
+            //Console.WriteLine("soz1-" + soz1);
+
+
+            // donguler
+            //int numb = 6;
+
+            //for (int i = 0; i<=10 ; i++)
+            //{
+            //    //if (i % 2 == 0)
+            //    //{
+            //    //    Console.WriteLine("i cut ededdir" + i);
+            //    //}
+            //    //else
+            //    //{
+            //    //    Console.WriteLine("i tek ededdir" + i);
+            //    //}
+            //    Console.WriteLine("i  ededdir" + i);
+
+            //    if (i == 6)
+            //    {
+            //        //continue;
+            //        break;
+            //    }
+            //}
+            ////Console.WriteLine("dovr bitdi");
+
+
+
+            //while
+
+            //int num = 2;
+
+
+            //while (num < 2)
+            //{
+            //    Console.WriteLine(num);
+            //    num++;
+            //}
+
+
+            //do
+            //{
+            //    Console.WriteLine(num);
+            //    num++;
+
+            //}
+            //while (num < 1);
+
+
+            Console.WriteLine("Reqemi daxil edin");
+            string ay=Console.ReadLine();   
+
+
+            switch(ay) {
+
+                case "1":
+                    Console.WriteLine("yanvar ayi");
+                    break;
+
+                case "2":
+                    Console.WriteLine("fevral ayi");
+                    break;
+                case "3":
+                    Console.WriteLine("Mart ayi");
+                    break;
+                case "4":
+                    Console.WriteLine("Aprel ayi");
+                    break;
+                default:
+
+                    Console.WriteLine("daxil etdiyiniz simvola uygun ay yoxdur");
+                    break;
+
+            }
+
+        }
+    }
+}
